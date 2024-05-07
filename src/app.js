fe865e355aa7eb13c09295f58bdb5db6b4c97355
Seven <sevenminaya@gmail.com>
2024-05-07 00:34:04 -0400

    Initial reset and connection to the API that provides the key

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const todosRouter = require('./routes/todos');
 
 const app = express();
 
@@ -13,8 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Routes
-app.use('/todos', todosRouter);
 
 
 module.exports = app;
